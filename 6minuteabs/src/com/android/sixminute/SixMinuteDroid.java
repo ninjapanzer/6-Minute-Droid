f8b10b4d342bff76dd2725b96837f15d32fe9800
ninjapanzer@gmail.com <ninjapanzer@gmail.com@387eb89e-9c7a-c29c-1410-a09b4bacf79b>
2010-04-17 22:24:09 +0000

    regenerated
    git-svn-id: https://6minutedroid.googlecode.com/svn@7 387eb89e-9c7a-c29c-1410-a09b4bacf79b

---
+++ 6minuteabs/src/com/android/sixminute/SixMinuteDroid.java
@@ -0,0 +1,13 @@
+package com.android.sixminute;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SixMinuteDroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
