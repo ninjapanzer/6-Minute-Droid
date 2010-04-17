d31719d1e27063599f8fc441e7c868a7172d7cfd
ninjapanzer@gmail.com <ninjapanzer@gmail.com@387eb89e-9c7a-c29c-1410-a09b4bacf79b>
2010-04-17 21:57:27 +0000

    entry point for activity
    git-svn-id: https://6minutedroid.googlecode.com/svn@5 387eb89e-9c7a-c29c-1410-a09b4bacf79b

---
+++ 6minuteabs/src/android/sixminuteabs/Sixmin.java
@@ -0,0 +1,12 @@
+package android.sixminuteabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Sixmin extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
